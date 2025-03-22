2cf87a55e6ce0b1779471199671f7bf55cc62413
zmahmood4 <z.mahmood4@hotmail.com>
2025-03-22 19:14:51 -0400

    linting checks

--- server/database/app.js
+++ server/database/app.js
@@ -23,10 +23,10 @@ const Dealerships = require("./dealership");
 
 try {
   Reviews.deleteMany({}).then(() => {
-    Reviews.insertMany(reviews_data["reviews"]);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(() => {
-    Dealerships.insertMany(dealerships_data["dealerships"]);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
 } catch (error) {
   res.status(500).json({ error: "Error fetching documents" });
@@ -91,18 +91,18 @@ app.get("/fetchDealer/:id", async (req, res) => {
 app.post("/insert_review", express.raw({ type: "*/*" }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort({ id: -1 });
-  let new_id = documents[0]["id"] + 1;
+  let new_id = documents[0].id + 1;
 
   const review = new Reviews({
     id: new_id,
-    name: data["name"],
-    dealership: data["dealership"],
-    review: data["review"],
-    purchase: data["purchase"],
-    purchase_date: data["purchase_date"],
-    car_make: data["car_make"],
-    car_model: data["car_model"],
-    car_year: data["car_year"],
+    name: data.name,
+    dealership: data.dealership,
+    review: data.review,
+    purchase: data.purchase,
+    purchase_date: data.purchase_date,
+    car_make: data.car_make,
+    car_model: data.car_model,
+    car_year: data.car_year,
   });
 
   try {
