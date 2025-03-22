4243f17586b5e3e94791a2a6bc1e44613bbcbe4e
zmahmood4 <z.mahmood4@hotmail.com>
2025-03-22 18:57:56 -0400

    linting

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0" # noqa: E501
+SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0"  # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
     "https://zmahmood4-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",  # noqa: E501
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://zmahmood4-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" # noqa: E501
+    "https://zmahmood4-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",  # noqa: E501
 ]
 
 REST_FRAMEWORK = {
